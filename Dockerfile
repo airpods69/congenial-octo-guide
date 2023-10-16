4821f8fc801f6d6eb2a05b9184598fc344909e03
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2023-10-16 02:05:33 -0400

    test files and docker

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
+FROM python:3.12.0
 
+RUN pip3 install transformers ctransformers ctransformers[cuda]
+
+CMD ["nvidia-smi"]
---
+++ app/llm/test_ctransformer.py
@@ -0,0 +1,7 @@
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained("marella/gpt-2-ggml")
+text = llm("AI will take over the world and ", stream=True)
+
+for word in text:
+    print(word, end = "", flush = True)
