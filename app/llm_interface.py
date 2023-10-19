d6cba17a3128a9600c48d0b13905bcfbf40c78d9
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2023-10-19 12:21:46 +0530

    Simple UI

--- app/llm_interface.py
+++ app/llm_interface.py
@@ -8,8 +8,11 @@ llm_factory = LLMFactory()
 llm_factory.register_type("ctransformer", CTRANSFORMERS_LLM)
 llm_factory.register_type("llamacpp", LLAMA_CPP_BASE)
 
-llm = llm_factory.get_llm("llamacpp","/home/varoo/Models/luna-ai-llama2-uncensored.Q5_0.gguf", gpu_layers=100, context_length=2048)
+llm = llm_factory.get_llm("llamacpp","/mnt/storage/RandomCS/Models/LLM/models/tinyllama-1.1b-1t-Q4gguf/tinyllama-1.1b-intermediate-step-480k-1t.Q4_K_S.gguf", gpu_layers=100, context_length=2048)
 
-print(llm.generate(f"Given to you is a document.\n{qpr_data}Summarize the critical details of this document into listed points. All the details are to be taken from the document only.", use_template=True, temperature = 0.4))
-print(llm.generate(f"Given to you is a document.\n{qpr_data}What tag/label will you give this document so that user can identify what this document is about?", use_template=True, temperature = 0.4))
+def generate_reply(prompt: str, temperature = 0.4, use_template = True) -> str:
+    return llm.generate(prompt, temperature = temperature, use_template = use_template)
+
+# print(llm.generate(f"Given to you is a document.\n{qpr_data}Summarize the critical details of this document into listed points. All the details are to be taken from the document only.", use_template=True, temperature = 0.4))
+# print(llm.generate(f"Given to you is a document.\n{qpr_data}What tag/label will you give this document so that user can identify what this document is about?", use_template=True, temperature = 0.4))
 # print(llm.generate(f"How are you doing today?", use_template=True))
